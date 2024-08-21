8b791f310bf4f7e1128b04a4dcd3aca832141a0e
Viktorline <viktorlinework@gmail.com>
2024-08-21 23:06:15 +0700

    004-add-some-template

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,32 +1,81 @@
-import { useState } from 'react';
+import cn from 'classnames'
+import { useState } from 'react'
+
+import styles from './App.module.css'
+
+type Pages = 'Instructions' | 'Settings'
 
 function App() {
-  const [color, setColor] = useState('');
+  const [activeSection, setActiveSection] = useState<Pages>('Instructions')
 
-  const onClick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript<string[], void>({
-      target: { tabId: tab.id! },
-      args: [color],
-      func: (color) => {
-        document.body.style.backgroundColor = color;
-      },
-    });
-  };
+  const handleMenuClick = (page: Pages) => {
+    setActiveSection(page)
+  }
 
   return (
-    <>
-      <h1>Test</h1>
-      <div className='card'>
-        <input
-          type='color'
-          value={color}
-          onChange={(e) => setColor(e.currentTarget.value)}
-        />
-        <button onClick={onClick}>do it</button>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <ul className={styles.menuList}>
+              <li className={styles.instruction}>
+                <button
+                  className={styles.instructionButton}
+                  onClick={() => handleMenuClick('Instructions')}
+                >
+                  Instructions
+                </button>
+              </li>
+              <li className={styles.instruction}>
+                <button
+                  className={styles.instructionButton}
+                  onClick={() => handleMenuClick('Settings')}
+                >
+                  Settings
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main className={styles.main}>
+          <section
+            className={cn(styles.sidebar, {
+              [styles.sidebarOpen]: activeSection === 'Settings'
+            })}
+          >
+            <div>Settings Content</div>
+          </section>
+
+          <ul className={styles.instructionsList}>
+            <li className={styles.instruction}>
+              <button className={styles.instructionButton}>
+                Translate to Russian
+              </button>
+            </li>
+            <li className={styles.instruction}>
+              <button className={styles.instructionButton}>
+                In development ...
+              </button>
+            </li>
+            <li className={styles.instruction}>
+              <button className={styles.instructionButton}>
+                In development ...
+              </button>
+            </li>
+          </ul>
+
+          <section
+            className={cn(styles.sidebar, {
+              [styles.sidebarOpen]: activeSection === 'Settings'
+            })}
+          >
+            <div>Settings Content</div>
+          </section>
+        </main>
       </div>
-    </>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+
+interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {}
+
+export function Button({}: ButtonProps) {
+  return (
+    <>
+      <button />
+    </>
+  )
+}
